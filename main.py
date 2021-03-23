f97840bfb6fcce4f21ee840668f1644451a11ee3
Alexandre Andrade <alex.d.a_14@outlook.pt>
2021-03-23 11:52:17 +0000

    adicionada robustez

--- main.py
+++ main.py
@@ -11,19 +11,30 @@ def login():
     print("login or signup? (l/s)")
     option=input()
     if option=='s':
-        print("Insira o seu username, email e password (separados por espaco):")
-        info_user=input().split(" ")
-        cursor.execute('''INSERT INTO utilizadores (username, email, password) VALUES (%s, %s, %s);''', (info_user[0], info_user[1], info_user[2]))
-        connection.commit()
+        while(1):
+            print("Insira o seu username, email e password (separados por espaco):")
+            info_user=input().split(" ")
+            try:
+                cursor.execute('''INSERT INTO utilizadores (username, email, password) VALUES (%s, %s, %s);''', (info_user[0], info_user[1], info_user[2]))
+                connection.commit()
+                print("Registado com sucesso!")
+                break
+            except:
+                print("Informacoes com sintaxe incorreta!")
     elif option=='l':
-        print("Insira o seu username e password (separados por espaco):")
-        info_user = input().split(" ")
-        cursor.execute('''SELECT COUNT(username) FROM utilizadores WHERE username=%s AND password=%s;''', (info_user[0], info_user[1]))
-        validation=cursor.fetchone()
-        if validation[0]==1:
-            print("Autenticado!")
-        else:
-            print("Combinacao de username e password inexistente")
+        while(1):
+            print("Insira o seu username e password (separados por espaco):")
+            info_user = input().split(" ")
+            try:
+                cursor.execute('''SELECT COUNT(username) FROM utilizadores WHERE username=%s AND password=%s;''', (info_user[0], info_user[1]))
+                validation=cursor.fetchone()
+                if validation[0]==1:
+                    print("Autenticado!")
+                    break
+                else:
+                    print("Combinacao de username e password inexistente, tente outra vez")
+            except:
+                print("Informacoes com sintaxe incorreta")
 
 if __name__ == '__main__':
     main()
