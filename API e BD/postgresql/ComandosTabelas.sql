b66db27fec071ec84ee8c6fadbe5a6d1350e31fc
Alexandre Andrade <alex.d.a_14@outlook.pt>
2021-05-27 18:52:09 +0100

    adicionado trigger

--- API e BD/postgresql/ComandosTabelas.sql
+++ API e BD/postgresql/ComandosTabelas.sql
@@ -95,3 +95,31 @@ BEGIN
 	return v_username;
 END;
 $$;
+
+--ATUALIZAR PRECO TABELA LEILAO NO FIM DE ADICIONAR ENTRADA NA TABELA LICITACAO
+create or replace function atualiza_preco_leilao()
+returns trigger
+language plpgsql
+as $$
+declare
+	c1 cursor for
+		select leilao_id, valor from licitacao where id = (select last_value from licitacao_id_seq);
+	v_valor INTEGER;
+	v_leilao_id INTEGER;
+begin
+	open c1;
+	fetch c1 into
+	v_leilao_id, v_valor;
+	UPDATE leilao
+	precoatual = v_valor
+	WHERE id = v_leilao_id;
+	return new;
+end;
+$$;
+
+create trigger trigger_atualizar_precoatual
+after insert on licitacao
+for each row
+execute procedure atualiza_preco_leilao();
+
+----------------
